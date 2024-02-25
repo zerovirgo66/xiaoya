8bd4580bc3000dae781136091e642121367a6a7e
zerovirgo66 <153780353+zerovirgo66@users.noreply.github.com>
2024-02-25 20:37:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.hicairo.com'
+      url.hostname = 'media.scca.site:5678'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
